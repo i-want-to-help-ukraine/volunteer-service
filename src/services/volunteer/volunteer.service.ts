853f5c04d4d855d0bcb790d683c8db26d0907a2a
Alexey Yudin <alexey.y.yudin@gmail.com>
2022-03-09 01:04:16 +0100

    feat: volunteer filters

--- src/services/volunteer/volunteer.service.ts
+++ src/services/volunteer/volunteer.service.ts
@@ -1,10 +1,16 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import {
+  ActivityDto,
+  CityDto,
+  PaymentProviderDto,
   SearchVolunteersRequest,
   VolunteerDto,
+  VolunteerPaymentOptionDto,
+  VolunteerSocialDto,
+  SocialProviderDto,
 } from '@i-want-to-help-ukraine/protobuf/types/volunteer-service';
-import { Volunteer } from '@prisma/client';
+import { Volunteer, City, VolunteerPaymentOption } from '@prisma/client';
 
 @Injectable()
 export class VolunteerService {
@@ -16,13 +22,95 @@ export class VolunteerService {
     request: SearchVolunteersRequest,
   ): Promise<VolunteerDto[] | null> {
     try {
-      const { cityIds } = request;
-
-      const volunteers = await this.prisma.volunteer.findMany({
-        where: {},
-      });
+      const volunteers = await this.prisma.volunteer.findMany({});
 
       return volunteers.map((volunteer) => this.mapVolunteer(volunteer));
+    } catch (e) {
+      this.logger.error(e);
+      return null;
+    }
+  }
+
+  async getCities(): Promise<City[]> {
+    try {
+      return this.prisma.city.findMany({});
+    } catch (e) {
+      this.logger.error(e);
+      return null;
+    }
+  }
+
+  async getSocialProviders(): Promise<SocialProviderDto[]> {
+    try {
+      return this.prisma.socialProvider.findMany({});
+    } catch (e) {
+      this.logger.error(e);
+      return null;
+    }
+  }
+
+  async getActivities(): Promise<ActivityDto[]> {
+    try {
+      return this.prisma.activity.findMany({});
+    } catch (e) {
+      this.logger.error(e);
+      return null;
+    }
+  }
+
+  async getPaymentProviders(): Promise<PaymentProviderDto[]> {
+    try {
+      return this.prisma.paymentProvider.findMany({});
+    } catch (e) {
+      this.logger.error(e);
+      return null;
+    }
+  }
+
+  async getVolunteerCities(): Promise<CityDto[]> {
+    try {
+      return this.prisma.city.findMany({});
+    } catch (e) {
+      this.logger.error(e);
+      return null;
+    }
+  }
+
+  async getVolunteerActivities(): Promise<ActivityDto[]> {
+    try {
+      return this.prisma.activity.findMany({});
+    } catch (e) {
+      this.logger.error(e);
+      return null;
+    }
+  }
+
+  async getVolunteerSocial(volunteerId: string): Promise<VolunteerSocialDto[]> {
+    try {
+      return this.prisma.volunteerSocial.findMany({
+        where: {
+          volunteerId,
+        },
+      });
+    } catch (e) {
+      this.logger.error(e);
+      return null;
+    }
+  }
+
+  async getVolunteerPaymentOptions(
+    volunteerId: string,
+  ): Promise<VolunteerPaymentOptionDto[]> {
+    try {
+      const paymentOptions = await this.prisma.volunteerPaymentOption.findMany({
+        where: {
+          volunteerId,
+        },
+      });
+
+      return paymentOptions.map((paymentOption) =>
+        this.mapVolunteerPaymentOption(paymentOption),
+      );
     } catch (e) {
       this.logger.error(e);
       return null;
@@ -46,15 +134,30 @@ export class VolunteerService {
     }
   }
 
+  private mapVolunteerPaymentOption(
+    volunteerPaymentOption: VolunteerPaymentOption,
+  ): VolunteerPaymentOptionDto {
+    const { id, volunteerId } = volunteerPaymentOption;
+
+    return {
+      id,
+      metadata: '',
+      paymentProviders: null,
+      volunteerId,
+    };
+  }
+
   private mapVolunteer(volunteer: Volunteer): VolunteerDto {
-    const { id, name } = volunteer;
+    const { id, name, verificationState } = volunteer;
 
     return {
       id,
       name,
-      cityIds: [],
-      activityIds: [],
-      paymentOptionIds: [],
+      verificationState,
+      cities: null,
+      activities: null,
+      social: null,
+      paymentOption: null,
     };
   }
 }
