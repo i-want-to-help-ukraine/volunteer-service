16ec31881b48785a2c992590fd46bb2c64f350c1
Alexey Yudin <alexey.y.yudin@gmail.com>
2022-05-03 14:01:00 +0200

    Release/rc 26042022 (#10)
    * feat: add volunteer providers
    
    * feat: volunteer pagination
    
    * feat: volunteer search pagination
    
    * ci: version bump to v0.5.1
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

--- src/services/volunteer/volunteer.service.ts
+++ src/services/volunteer/volunteer.service.ts
@@ -2,22 +2,26 @@ import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import {
   ActivityDto,
+  AddActivityDto,
+  AddContactProviderDto,
+  AddPaymentProviderDto,
+  AddSocialProviderDto,
   CityDto,
+  ContactProviderDto,
+  CreateProfileDto,
   PaymentProviderDto,
+  SearchVolunteersDto,
+  SocialProviderDto,
+  UpdateProfileDto,
+  VolunteerContactDto,
   VolunteerDto,
   VolunteerPaymentOptionDto,
   VolunteerSocialDto,
-  SocialProviderDto,
-  CreateProfileDto,
-  UpdateProfileDto,
-  SearchVolunteersDto,
-  VolunteerContactDto,
-  ContactProviderDto,
 } from '@i-want-to-help-ukraine/protobuf/types/volunteer-service';
 import {
   Volunteer,
-  VolunteerPaymentOption,
   VolunteerContact,
+  VolunteerPaymentOption,
   VolunteerSocial,
 } from '@prisma/client';
 import { VerificationStatus } from '../../enums/verification-status';
@@ -33,14 +37,15 @@ export class VolunteerService {
     volunteers: VolunteerDto[];
   }> {
     try {
-      const { cityIds, activityIds, offset, startCursor } = request;
+      const { cityIds, activityIds, count, offset, startCursor } = request;
 
       if (cityIds.length === 0 && activityIds.length === 0) {
         const volunteers = await this.prisma.volunteer.findMany({
           orderBy: {
             createdAt: 'desc',
           },
-          take: offset + 1,
+          skip: offset,
+          take: count,
           cursor: startCursor
             ? {
                 id: startCursor,
@@ -63,7 +68,8 @@ export class VolunteerService {
         orderBy: {
           createdAt: 'asc',
         },
-        take: offset + 1,
+        skip: offset,
+        take: count,
         cursor: startCursor
           ? {
               id: startCursor,
@@ -639,6 +645,72 @@ export class VolunteerService {
       this.logger.error(e);
 
       return [];
+    }
+  }
+
+  async addActivity({
+    title,
+    description,
+  }: AddActivityDto): Promise<ActivityDto | null> {
+    try {
+      return this.prisma.activity.create({
+        data: {
+          title,
+          description,
+        },
+      });
+    } catch (e) {
+      this.logger.error(e);
+
+      return null;
+    }
+  }
+
+  async addPaymentProvider({
+    title,
+  }: AddPaymentProviderDto): Promise<PaymentProviderDto | null> {
+    try {
+      return this.prisma.paymentProvider.create({
+        data: {
+          title,
+        },
+      });
+    } catch (e) {
+      this.logger.error(e);
+
+      return null;
+    }
+  }
+
+  async addContactProvider({
+    title,
+  }: AddContactProviderDto): Promise<ContactProviderDto | null> {
+    try {
+      return this.prisma.contactProvider.create({
+        data: {
+          title,
+        },
+      });
+    } catch (e) {
+      this.logger.error(e);
+
+      return null;
+    }
+  }
+
+  async addSocialProvider({
+    title,
+  }: AddSocialProviderDto): Promise<SocialProviderDto | null> {
+    try {
+      return this.prisma.socialProvider.create({
+        data: {
+          title,
+        },
+      });
+    } catch (e) {
+      this.logger.error(e);
+
+      return null;
     }
   }
 
